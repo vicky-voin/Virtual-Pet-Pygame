a5b5331a5d0ac6245badc470712ab0c37d0c5705
Vicky V <alteneria127@gmail.com>
2025-02-07 12:06:16 -0500

    Added a couple more tests for loading

--- tests/pet_test.py
+++ tests/pet_test.py
@@ -7,5 +7,13 @@ class PetTests(unittest.TestCase):
         self.assertIsNotNone(pet_instance.type)
         self.assertNotEqual(pet_instance.type, "")
 
+    @unittest.expectedFailure
+    def test_setup_pet_wrong_path_fail(self):
+        pet_instance = Pet.load("wrongPath")
+
+    @unittest.expectedFailure
+    def test_setup_pet_json_mismatch_fail(self):
+        pet_instance = Pet.load("tests/test_data/test_pet_corrupted.json")
+
 if __name__ == '__main__':
     unittest.main()
