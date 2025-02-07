643933e8de3e3823c11107eeba5a9398162850ac
Vicky V <alteneria127@gmail.com>
2025-02-07 12:01:34 -0500

    Pet data is loaded from JSON

--- pet.py
+++ pet.py
@@ -1,6 +1,16 @@
+import json
+import os
 
 class Pet:
-    @classmethod
-    def load(cls, data_path):
-        cls.petType = "testType"
-        return cls
+    
+    type = ""
+
+    @staticmethod
+    def load(data_path):
+        main_dir = os.path.split(os.path.abspath(__file__))[0]
+        data_dir = os.path.join(main_dir, data_path)
+        data = open(data_dir,)
+        
+        pet = Pet()
+        pet.__dict__ = json.load(data)
+        return pet
--- tests/pet_test.py
+++ tests/pet_test.py
@@ -4,7 +4,8 @@ from pet import Pet
 class PetTests(unittest.TestCase):
     def test_setup_pet_success(self):
         pet_instance = Pet.load("tests/test_data/test_pet.json")
-        self.assertIsNotNone(pet_instance.petType)
+        self.assertIsNotNone(pet_instance.type)
+        self.assertNotEqual(pet_instance.type, "")
 
 if __name__ == '__main__':
     unittest.main()
